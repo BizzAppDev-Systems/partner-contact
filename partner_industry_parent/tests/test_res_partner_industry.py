ac480b3096116259005215608b02bdcd7e73ed08
Ruchir Shukla <ruchir@bizzappdev.com>
2026-01-07 16:40:05 +0530

    [MIG] partner_industry_parent: Migration to 19.0

--- partner_industry_parent/tests/test_res_partner_industry.py
+++ partner_industry_parent/tests/test_res_partner_industry.py
@@ -1,15 +1,16 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestResPartnerIndustry(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.industry_1 = self.env.ref("base.res_partner_industry_O")
-        self.industry_2 = self.env.ref("base.res_partner_industry_P")
-        self.industry_3 = self.env.ref("base.res_partner_industry_Q")
+class TestResPartnerIndustry(BaseCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.industry_1 = cls.env.ref("base.res_partner_industry_O")
+        cls.industry_2 = cls.env.ref("base.res_partner_industry_P")
+        cls.industry_3 = cls.env.ref("base.res_partner_industry_Q")
 
     def test_res_partner_industry_complete_name(self):
         self.assertEqual(self.industry_1.complete_name, "Public Administration")
